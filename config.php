10a562fbe0d5cf67fecfba754ed6539914813ae0
Tri Phan <pttri89@gmail.com>
2016-12-29 15:34:29 +0700

    [TriPhan] config.php: change SITE_URL to 'http://demoaut.katalon.com/'

--- config.php
+++ config.php
@@ -18,7 +18,7 @@ define('TPL_DIR', __DIR__ . '/views');
 
 // Site config
 // define('SITE_URL', "http://localhost/katalon-sample/"); // website URL with ended slash
-define('SITE_URL', $_SERVER['SITE_URL']); // website URL with ended slash
+define('SITE_URL', "http://demoaut.katalon.com/");
 define('SITE_NAME', "CURA Healthcare Service");
 define('SITE_ADDRESS', "Atlanta 550 Pharr Road NE Suite 525<br>Atlanta, GA 30305");
 define('SITE_TEL', "(678) 813-1KMS");
