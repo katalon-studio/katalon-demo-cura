05707c6b73b9683471b4106998f903239d6c8682
Alex Smith <dev.alex.88@gmail.com>
2019-04-03 09:41:50 +0700

    update

--- views/page_template.php
+++ views/page_template.php
@@ -36,24 +36,24 @@
     <ul class="sidebar-nav">
         <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
         <li class="sidebar-brand">
-            <a href="./" onclick=$("#menu-close").click();>CURA Healthcare</a>
+            <a href="./" onclick="$('#menu-close').click();">CURA Healthcare</a>
         </li>
         <li>
-            <a href="./" onclick=$("#menu-close").click();>Home</a>
+            <a href="./" onclick="$('#menu-close').click();">Home</a>
         </li>
         <?php if (_f::is_user_logged_in()) : ?>
         <li>
-            <a href="history.php#history" onclick=$("#menu-close").click();>History</a>
+            <a href="history.php#history" onclick="$('#menu-close').click();">History</a>
         </li>
         <li>
-            <a href="profile.php#profile" onclick=$("#menu-close").click();>Profile</a>
+            <a href="profile.php#profile" onclick="$('#menu-close').click();">Profile</a>
         </li>
         <li>
-            <a href="authenticate.php?logout" onclick=$("#menu-close").click();>Logout</a>
+            <a href="authenticate.php?logout" onclick="$('#menu-close').click();">Logout</a>
         </li>
         <?php else: ?>
         <li>
-            <a href="profile.php#login" onclick=$("#menu-close").click();>Login</a>
+            <a href="profile.php#login" onclick="$('#menu-close').click();">Login</a>
         </li>
         <?php endif; ?>
     </ul>
